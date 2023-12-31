e6ff05208fbb15dd0322de5c54513a0e2436533f
German <lordireality@gmail.com>
2023-12-31 06:45:36 +0300

    Initial Commit

---
+++ Silly App/Silly_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Silly_AppApp.swift
+//  Silly App
+//
+//  Created by Герман Зыкин on 31.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Silly_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
